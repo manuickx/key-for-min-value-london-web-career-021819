f952711b9cda614b66e3400812ec83b15571f393
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-07 00:57:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 
 def key_for_min_value(name_hash)
   min_val = name_hash.first[1]
-  min_key = name_hash.first[0]
-  name_hash.each do |key, val|
-    if value < min_val
-      min_val = value
-      min_key = key
+  min_name = name_hash.first[0]
+  name_hash.each do |name, val|
+    if val < min_val
+      min_val = val
+      min_name = name
     end
   end
-  min_key
+  min_name
 end
