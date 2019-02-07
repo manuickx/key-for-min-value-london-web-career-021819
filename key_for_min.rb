8b3f933e6a58b76a5e87e7800090ee6b8868766f
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-07 00:55:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |item, value|
-    
-
+  min_val = name_hash.first[1]
+  min_key = name_hash.first[0]
+  name_hash.each do |key, val|
+    if value < min_val
+      min_val = value
+      min_key = key
+    end
+  end
+  min_key
 end
